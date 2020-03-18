de47b397310b30f312c4f420c975fc92b8a6405d
KalyanandaVedula <bharatvedula@gmail.com>
2020-03-18 17:47:37 +1100

    Styling Changes

--- react-native-login-template/src/index.js
+++ react-native-login-template/src/index.js
@@ -2,13 +2,14 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import {LoginScreen} from './screens';
-import {HomeScreen,Dashboard} from './screens';
+import {HomeScreen,Dashboard,BurgerMenu} from './screens';
 
 const Router = createStackNavigator(
   {
     HomeScreen,
     LoginScreen,
     Dashboard,
+    BurgerMenu,
  
    
   },
