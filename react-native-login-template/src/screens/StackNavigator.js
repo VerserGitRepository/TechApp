be125c68d50ee31d486aa4fd125dfb9b555ce62a
KalyanandaVedula <bharatvedula04@gmail.com>
2020-03-31 13:02:42 +1100

    navigation

---
+++ react-native-login-template/src/screens/StackNavigator.js
@@ -0,0 +1,14 @@
+import React, { memo, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
+
+const _onSStackNavigator = () => {
+    const Stack1 = '';
+    const Stack2 = '';
+    const Stack3 = '';
+
+    
+
+    navigation.navigate('StackNavigator');
+  };
